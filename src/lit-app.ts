3012a4b445294f7f0b90962ce3cef25cdb938680
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2022-06-03 16:08:55 +0530

    my-first-lit-app

--- src/lit-app.ts
+++ src/lit-app.ts
@@ -1,5 +1,5 @@
-import { LitElement, html, css } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { LitElement, html, css, PropertyValueMap } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
 
 @customElement('lit-app')
 export class LitApp extends LitElement {
@@ -12,8 +12,30 @@ export class LitApp extends LitElement {
     ];
 
     @property() name = 'Lit App';
+    @state() count = 0;
+    @state() isDisabled = false;
 
+    increment() {
+        this.count++;
+        if (this.count > 0) {
+            this.isDisabled = false;
+        }
+    }
+
+    decrement() {
+        if(this.count > 0) {
+            this.count--;
+        }
+        if (this.count === 0) {
+            this.isDisabled = true;
+        }
+    }
+    
     render() {
-        return html`<h1>Hello, ${this.name}</h1>`;
+        return html`<h1>Hello, ${this.name}</h1>
+            <p>Count: ${this.count}</p>
+            <button @click=${this.increment}>Increment</button>
+            <button ?disabled=${this.isDisabled} @click=${this.decrement}>Decrement</button>
+        `;
     }
 }
